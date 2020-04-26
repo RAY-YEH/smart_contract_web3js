0e45676e1908bbf6e4ccd96fbc887d253977e32f
RAY-YEH <64343616+RAY-YEH@users.noreply.github.com>
2020-04-26 13:42:11 +0800

    Update lunch.js

--- lunch.js
+++ lunch.js
@@ -103,8 +103,8 @@ abi =[
 ]
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x0382F9Bc0D2fecA5C95669203bdd09dF5481d397');
-candidates = {'黑咖啡': 'candidate-1', '拿鐵咖啡': 'candidate-2', '卡布奇諾': 'candidate-3'}
+contractInstance = VotingContract.at('0x5ae37D884b40e6903d0D39717f6D7c75d70aF256');
+candidates = {'棒球': 'candidate-1', '籃球': 'candidate-2', '足球': 'candidate-3'}
 
 function voteForCandidate() {
   candidateName = $('#candidate').val();
